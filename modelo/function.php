8f210d1b806e59d4de32b8476368db3af79d58cf
erichenrique77 <erichenrique99911@gmailcom>
2022-02-21 11:51:36 -0300

    arquivos

---
+++ modelo/function.php
@@ -0,0 +1,33 @@
+<?php
+$NOME = $_REQUEST['NOME'];
+$ANIMAL = $_REQUEST['ANIMAL'];
+
+if (empty($NOME)){
+    $dados = array(
+        "tipo" => "error",
+        "mensagem" => "Existe(em) campo(s) a ser(em) preenchido(s)."
+    );
+} else {
+
+    switch($ANIMAL){
+        case '1' : $dados = array(
+            "tipo" => "Cachorro",
+            "mensagem" => "Seja bem-vindo, '.$NOME', sabemos que o seu animal favorito é o cachorro"
+        );
+        break;
+
+        case '2' : $dados = array(
+            "tipo" => "Gato",
+            "mensagem" => "Seja bem-vindo, '.$NOME', sabemos que o seu animal favorito é o Gato"
+        );
+        break;
+
+        case '3' : $dados = array(
+            "tipo" => "Peixe",
+            "mensagem" => "Seja bem-vindo, '.$NOME', sabemos que o seu animal favorito é o Peixe"
+        );
+        break;
+    }
+}
+
+echo json_encode($dados);
